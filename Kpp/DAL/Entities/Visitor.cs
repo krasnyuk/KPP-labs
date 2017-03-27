170e0900d877c6019c6f5c26227ff246f05160cd
krasnyuk <krasnyuk.photo@gmail.com>
2017-03-27 23:56:50 +0300

    DAL: context

--- Kpp/DAL/Entities/Visitor.cs
+++ Kpp/DAL/Entities/Visitor.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DAL
 {
-    class Visitor
+    public class Visitor
     {
+        public int VisitorId { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public double Age { get; set; }
+        [Required]
+        public string Telephone { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string Feedback { get; set; }
+
     }
 }
---
+++ Kpp/DAL/KppInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class KppInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<KppContext>
+    {
+        protected override void Seed(KppContext context)
+        {
+            var students = new List<Visitor>
+            {
+                new Visitor(){
+                    LastName = "Krasnyuk",
+                    FirstName = "Artyom",
+                    Age = 1,
+                    Email = "krasnyuk.photo@gmail.com",
+                    Gender = "Male",
+                    Telephone = "+38068 25 06 773",
+                    Feedback = "Good!"}
+            };
+
+            students.ForEach(s => context.Visitors.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
